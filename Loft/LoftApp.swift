54f1485d1595780ac769a1977605f2e367e3c62a
cute_catgirl <benpmoon@gmail.com>
2024-11-11 10:24:30 -0700

    Initial Commit

---
+++ Loft/LoftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoftApp.swift
+//  Loft
+//
+//  Created by Ben on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LoftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
