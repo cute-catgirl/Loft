a6470c17c3d60b8248daf7aa8347d4a3d1914b15
cute_catgirl <benpmoon@gmail.com>
2024-11-15 19:45:04 -0700

    gif support

---
+++ Loft/Views/GifView.swift
@@ -0,0 +1,18 @@
+//
+//  GifView.swift
+//  Loft
+//
+//  Created by Ben on 11/15/24.
+//
+
+import SwiftUI
+
+struct GifView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GifView()
+}
